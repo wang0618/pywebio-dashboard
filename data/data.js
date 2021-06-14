7924f35948bda740e4111274c68d42d027e4cd10
wang0618 <wang0.618@qq.com>
2021-06-14 16:31:20 +0000

    Data update at Mon Jun 14 16:31:20 UTC 2021

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
 var data = {
-    "GithubStars": 1529,
+    "GithubStars": 1530,
     "GithubUsed": 204,
-    "DayNewStars": 1,
+    "DayNewStars": 2,
     "WeekCDNHits": 28252,
     "WeekNewUsed": 8,
     "star-dates": [
@@ -104,7 +104,7 @@ var data = {
         1523,
         1527,
         1528,
-        1529
+        1530
     ],
     "star-daily": [
         0,
@@ -155,7 +155,7 @@ var data = {
         2,
         4,
         1,
-        1
+        2
     ],
     "used-dates": [
         "2021-04-27",
