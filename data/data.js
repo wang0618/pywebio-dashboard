508e44fef5e48525199f53b639823743c61760cd
wang0618 <wang0.618@qq.com>
2021-07-15 00:58:50 +0000

    Data update at Thu Jul 15 00:58:50 UTC 2021

--- data/data.js
+++ data/data.js
@@ -1,11 +1,10 @@
 var data = {
     "GithubStars": 1606,
     "GithubUsed": 252,
-    "WeekNewStars": 17,
-    "WeekCDNHits": 15238,
-    "WeekNewUsed": 7,
+    "WeekNewStars": 13,
+    "WeekCDNHits": 14674,
+    "WeekNewUsed": 5,
     "star-dates": [
-        "2021-05-27",
         "2021-05-28",
         "2021-05-29",
         "2021-05-30",
@@ -53,10 +52,10 @@ var data = {
         "2021-07-11",
         "2021-07-12",
         "2021-07-13",
-        "2021-07-14"
+        "2021-07-14",
+        "2021-07-15"
     ],
     "star-total": [
-        1492,
         1494,
         1494,
         1497,
@@ -104,10 +103,10 @@ var data = {
         1602,
         1602,
         1602,
+        1606,
         1606
     ],
     "star-daily": [
-        7,
         2,
         0,
         3,
@@ -155,10 +154,10 @@ var data = {
         1,
         0,
         0,
-        4
+        4,
+        0
     ],
     "used-dates": [
-        "2021-05-27",
         "2021-05-28",
         "2021-05-29",
         "2021-05-30",
@@ -206,10 +205,10 @@ var data = {
         "2021-07-11",
         "2021-07-12",
         "2021-07-13",
-        "2021-07-14"
+        "2021-07-14",
+        "2021-07-15"
     ],
     "used-total": [
-        194,
         194,
         195,
         198,
@@ -257,10 +256,10 @@ var data = {
         252,
         252,
         252,
+        252,
         252
     ],
     "used-daily": [
-        4,
         0,
         1,
         3,
@@ -306,12 +305,12 @@ var data = {
         2,
         2,
         1,
+        0,
         0,
         0,
         0
     ],
     "cdn-dates": [
-        "2021-05-24",
         "2021-05-25",
         "2021-05-26",
         "2021-05-27",
@@ -360,10 +359,10 @@ var data = {
         "2021-07-09",
         "2021-07-10",
         "2021-07-11",
-        "2021-07-12"
+        "2021-07-12",
+        "2021-07-13"
     ],
     "cdn-daily": [
-        1763,
         1779,
         2213,
         4192,
@@ -412,10 +411,10 @@ var data = {
         4038,
         1044,
         885,
-        2604
+        2604,
+        2019
     ],
     "views-dates": [
-        "2021-06-30",
         "2021-07-01",
         "2021-07-02",
         "2021-07-03",
@@ -432,8 +431,7 @@ var data = {
         "2021-07-14"
     ],
     "views-count": [
-        1,
-        111,
+        96,
         172,
         109,
         90,
@@ -446,10 +444,9 @@ var data = {
         105,
         160,
         119,
-        126
+        147
     ],
     "views-uniques": [
-        1,
         26,
         48,
         29,
@@ -463,7 +460,7 @@ var data = {
         24,
         34,
         36,
-        43
+        45
     ],
     "clones-dates": [
         "2021-07-01",
@@ -485,7 +482,7 @@ var data = {
         2,
         1,
         6,
-        4
+        5
     ],
     "clones-uniques": [
         1,
@@ -496,28 +493,28 @@ var data = {
         2,
         1,
         5,
-        4
+        5
     ],
     "referrers": [
         {
             "referrer": "pywebio.readthedocs.io",
-            "count": 229,
-            "uniques": 137
+            "count": 216,
+            "uniques": 132
         },
         {
             "referrer": "Google",
-            "count": 136,
-            "uniques": 82
+            "count": 128,
+            "uniques": 78
         },
         {
             "referrer": "github.com",
-            "count": 98,
-            "uniques": 67
+            "count": 94,
+            "uniques": 64
         },
         {
             "referrer": "pyweb.io",
-            "count": 12,
-            "uniques": 11
+            "count": 11,
+            "uniques": 10
         },
         {
             "referrer": "news.ycombinator.com",
@@ -525,14 +522,14 @@ var data = {
             "uniques": 5
         },
         {
-            "referrer": "link.zhihu.com",
-            "count": 7,
-            "uniques": 6
+            "referrer": "analyticsindiamag.com",
+            "count": 6,
+            "uniques": 2
         },
         {
-            "referrer": "analyticsindiamag.com",
-            "count": 7,
-            "uniques": 2
+            "referrer": "link.zhihu.com",
+            "count": 5,
+            "uniques": 5
         },
         {
             "referrer": "DuckDuckGo",
@@ -553,31 +550,31 @@ var data = {
     "referrers-total-chart": [
         {
             "name": "pywebio.readthedocs.io",
-            "value": 229
+            "value": 216
         },
         {
             "name": "Google",
-            "value": 136
+            "value": 128
         },
         {
             "name": "github.com",
-            "value": 98
+            "value": 94
         },
         {
             "name": "pyweb.io",
-            "value": 12
+            "value": 11
         },
         {
             "name": "news.ycombinator.com",
             "value": 9
         },
         {
-            "name": "link.zhihu.com",
-            "value": 7
+            "name": "analyticsindiamag.com",
+            "value": 6
         },
         {
-            "name": "analyticsindiamag.com",
-            "value": 7
+            "name": "link.zhihu.com",
+            "value": 5
         },
         {
             "name": "DuckDuckGo",
@@ -595,31 +592,31 @@ var data = {
     "referrers-unique-chart": [
         {
             "name": "pywebio.readthedocs.io",
-            "value": 137
+            "value": 132
         },
         {
             "name": "Google",
-            "value": 82
+            "value": 78
         },
         {
             "name": "github.com",
-            "value": 67
+            "value": 64
         },
         {
             "name": "pyweb.io",
-            "value": 11
+            "value": 10
         },
         {
             "name": "news.ycombinator.com",
             "value": 5
         },
         {
-            "name": "link.zhihu.com",
-            "value": 6
-        },
-        {
             "name": "analyticsindiamag.com",
             "value": 2
+        },
+        {
+            "name": "link.zhihu.com",
+            "value": 5
         },
         {
             "name": "DuckDuckGo",
