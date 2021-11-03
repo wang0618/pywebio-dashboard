523ec701de945508d16dd94b4d03b6fea1745bfb
wangweimin <wang0.618@qq.com>
2021-11-03 20:57:00 +0800

    workflow: generate no more than one commit in one day

---
+++ push_web.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This script commit local change to `web` branch and push commit to origin repo.
+
+last_commit_date=$(git log -1 --date=format:"%Y/%m/%d" --format="%ad")
+today=$(date +"%Y/%m/%d")
+
+echo "Today: $today, last commit date: $last_commit_date"
+
+git checkout web
+git add .
+
+if [[ "$today" == "$last_commit_date" ]]; then
+  git commit --amend -m "Data update at `date`" || exit 0
+else
+  git commit -m "Data update at `date`" || exit 0
+fi
+
+git push -f -u origin || exit 0
